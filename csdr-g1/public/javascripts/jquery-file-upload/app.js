9e3fc2eaeaaebd9383edaed9e3135d08f3468bc8
Alex Hauer <alex.hauer@happyninja.net>
2014-06-16 19:40:09 +0200

    jQuery File Upload improvements (basic implementation, no custom styles, no backend implementation)

---
+++ csdr-g1/public/javascripts/jquery-file-upload/app.js
@@ -0,0 +1,18 @@
+/** *************** **/
+/** * File Upload * **/
+/** *************** **/
+$(document).ready(function () {
+    $("#donateForm").fileupload({
+	    url: '@routes.Donate.upload()',
+        sequentialUploads: true,
+        dataType: 'json',
+    	maxFileSize: 5000000,
+    	acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+        done: function (e, data) {
+	        alert(data);
+            /* $.each(data.result.files, function (index, file) {
+                $('<p/>').text(file.name).appendTo(document.body);
+            }); */
+        }
+    });
+});
