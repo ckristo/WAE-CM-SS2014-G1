710274e22e64c98cd487cb1ea53c13c7d00cf52f
Alex Hauer <alex.hauer@happyninja.net>
2014-06-17 19:50:38 +0200

    Frontend customizations for the file upload plugin

--- csdr-g1/gulpfile.js
+++ csdr-g1/gulpfile.js
@@ -9,23 +9,32 @@ var minifycss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var gulpIgnore = require('gulp-ignore');
+var minimatch = require('minimatch');
+
+// Paths
+var paths = {
+		css: ['public/sass/*.css'],
+		sass: ['public/sass/*.scss'],
+		scripts: ['public/javascripts/**/*.js', '!public/javascripts/libs/**', '!public/javascripts/dist/**']
+};
 
 // Lint
 gulp.task('lint', function() {
-    return gulp.src('public/javascripts/app/*.js')
+    return gulp.src(paths.scripts)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 //Copy vanilla CSS files
 gulp.task('css-copy', function() {
-	return gulp.src('public/sass/*.css')
+	return gulp.src(paths.css)
 		.pipe(gulp.dest('public/stylesheets'));
 });
 
 // Sass
 gulp.task('sass', function() {
-    return gulp.src('public/sass/*.scss')
+    return gulp.src(paths.sass)
         .pipe(sass({ style: 'expanded', }))
 	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest('public/stylesheets'))
@@ -36,12 +45,12 @@ gulp.task('sass', function() {
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('public/javascripts/*.js')
+    return gulp.src(paths.scripts)
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('public/javascripts/dist'))
         .pipe(rename('app.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('public/javascripts/dist'));
 });
 
 // Watch Files For Changes
--- csdr-g1/public/javascripts/fileupload.js
+++ csdr-g1/public/javascripts/fileupload.js
@@ -9,6 +9,13 @@ $(document).ready(function () {
         dataType: 'json',
     	maxFileSize: 5000000,
     	acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+    	added: function (e, data) {
+    		$("li.template-upload .btn-danger").click(function(event) {
+    			event.preventDefault();
+    			$(this).parents("li").fadeOut('fast');
+    			return false; // prevent form submission
+    		});
+        },
         done: function (e, data) {
 	        alert(data);
             /* $.each(data.result.files, function (index, file) {
